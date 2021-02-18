afe3e3e1c33577d7f6b6f23a4b0d59db124fb0af
John McNeil <johnmcneil@users.noreply.github.com>
2021-02-18 09:26:18 -0600

    Update 2021-02-18.py

---
+++ 2021-02-18.py
@@ -0,0 +1,11 @@
+# dictionaries
+# store data in key: value pairs
+# ordered, changeable, does not allow duplicates
+# ordered as of Python 3.7. Unordered in Python 3.6 and earlier.
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+
