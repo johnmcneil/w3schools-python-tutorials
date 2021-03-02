3ee90854528d74d1ea33fa0a5891c58839165da4
John McNeil <johnmcneil@users.noreply.github.com>
2021-03-02 07:46:01 -0600

    Create 2021-03-02.py

---
+++ 2021-03-02.py
@@ -0,0 +1,25 @@
+# variable scope
+x = 300
+
+
+def myfunc():
+    x = 200
+    print(x)
+
+
+myfunc()
+print(x)
+
+
+# global keyword
+x = 300
+
+
+def myfunc():
+    global x
+    x = 200
+    print(x)
+
+
+myfunc()
+print(x)
