823005cf236576ff5c14d192952508b4110eb9fb
John McNeil <johnmcneil@users.noreply.github.com>
2021-03-04 19:15:18 -0600

    Update 2021-03-04.py

--- 2021-03-04.py
+++ 2021-03-04.py
@@ -30,6 +30,57 @@ print(person1["age"])
 # i.e. not mymodule.person1["age"]
 
 
+# dates
+# date is not a data type in python
+# instead you can import the module datetime to work with date objects
+import datetime
+x = datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.strftime("%A"))
+
+
+# create a datetime object
+# use the datetime() class constructor
+# it requires 3 parameters:year, month, day
+# can also take parameters for time (default value 0) and timezone (default value None)
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+
+# strftime() method - for formatting date objects into readable strings.
+
+print(x.strftime("%B"))
+# strftime.net is a good resource for format codes.
+
+
+# Python Math
+# Python has built in math functions and a math module
+# built in math functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+z = abs(-15)
+a = pow(4, 3)
+
+print(x, y, z, a)
+
+# math module
+import math
+    x = math.sqrt(64)
+print(x)
+print(math.ceil(5.5))
+print(math.floor(5.5))
+print(math.pi)
+
+
+
+
+
+
+
+
+
+
 
 
 
