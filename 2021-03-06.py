ebee076463e4c08e1c76564edea6d0d02c1a1dbb
John McNeil <johnmcneil@users.noreply.github.com>
2021-03-06 14:03:08 -0600

    Update 2021-03-06.py

--- 2021-03-06.py
+++ 2021-03-06.py
@@ -52,9 +52,69 @@ except:
 
 
 # you can define what kind of error to raise
-x = "hello"
-if not type(x) is int:
-    raise TypeError("only integers allowed")
+# x = "hello"
+# if not type(x) is int:
+#    raise TypeError("only integers allowed")
+
+
+# user input
+# username = input("Enter username:")
+# print("Username is: " + username)
+# python stops executing when it comes to input() function, continues when the user has given some input
+
+
+# format() method - allows you to format selected parts of a string
+# using placeholders  {} in the text
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+
+# format price to be displayed as a number with two decimals
+txt = "The price is {:.2f} dollars"
+print(txt.format(price))
+
+# string format reference
+# https://www.w3schools.com/python/ref_string_format.asp
+
+# multiple placeholder values
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+
+# you can use index numbers to make sure values are in correct order
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# index number allows you to use the same value more than once
+age = 36
+name = "John"
+txt = "His name is {1}. {1} is {0} years old."
+print(txt.format(name, age))
+
+
+# you can name indexes in the {} and use them within the format method
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname="Ford", model="Mustang"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
